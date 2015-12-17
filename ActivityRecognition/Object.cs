65679991abd7bbe97e01d33afdd22bbbf4276308
DongyangY <dongyang.yao@rutgers.edu>
2015-12-17 18:32:32 -0500

    add object comments

--- ActivityRecognition/Object.cs
+++ ActivityRecognition/Object.cs
@@ -1,13 +1,34 @@
-﻿using System.ComponentModel;
+﻿//------------------------------------------------------------------------------
+// <summary>
+// Description for a tagged object
+// </summary>
+// <author> Dongyang Yao (dongyang.yao@rutgers.edu) </author>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel;
 
 namespace ActivityRecognition
 {
     public class Object : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Use status of the object
+        /// </summary>
         private bool isInUse;
+
+        /// <summary>
+        /// RSSI value for the object
+        /// </summary>
         private double rssi;
 
+        /// <summary>
+        /// Defined object types
+        /// </summary>
         public enum Objects { Book, Mouse, Cup, Marker, Bowl };
+
+        /// <summary>
+        /// Automatically change the display of use status 
+        /// </summary>
         public bool IsInUse 
         {
             get { return isInUse; }
@@ -17,10 +38,30 @@ namespace ActivityRecognition
                 if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("IsInUse")); 
             }
         }
+
+        /// <summary>
+        /// Object name
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Tag ID for the object
+        /// </summary>
         public string ID { get; set; }
+
+        /// <summary>
+        /// The sum of RSSI for a period
+        /// </summary>
         public double RSSIAccumularor;
+
+        /// <summary>
+        /// The read times for a period
+        /// </summary>
         public int ReadTimes;
+
+        /// <summary>
+        /// Automatically change the RSSI in display 
+        /// </summary>
         public double RSSI 
         {
             get { return rssi; } 
@@ -31,6 +72,11 @@ namespace ActivityRecognition
             } 
         }
 
+        /// <summary>
+        /// Constructor for object
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="id"></param>
         public Object(string name, string id)
         {
             Name = name;
@@ -41,6 +87,9 @@ namespace ActivityRecognition
             IsInUse = false;
         }
 
+        /// <summary>
+        /// handler for property changed
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
